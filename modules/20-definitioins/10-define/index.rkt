ff8856043941a9b3a4b94d0171334011436f0ea9
Kirill Mokevnin <mokevnin@gmail.com>
2019-03-17 20:16:52 +0400

    add new lessons

---
+++ modules/20-definitioins/10-define/index.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+#| BEGIN |#
+(define members-count 100)
+(display members-count)
+#| END |#
---
+++ modules/20-definitioins/20-lambda-call/index.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+#| BEGIN |#
+(define result ((lambda (num1 num2) (/ (+ num1 num2) 2))
+                2 4))
+(display result)
+#| END |#
