78b3528d25cd2c6b1e9900301ffbcaab69b7c7b9
Andrey <fakesupp@gmail.com>
2019-03-18 22:56:37 +0300

    update solution

--- modules/30-logic/10-boolean-type/index.rkt
+++ modules/30-logic/10-boolean-type/index.rkt
@@ -3,7 +3,7 @@
 (provide (all-defined-out))
 
 #| BEGIN |#
-(define (same-parity? x y) (or (and (odd? x) (odd? y))
-                               (and (even? x) (even? y))
+(define (same-parity? x y) (or (and (= (remainder x 2) 0) (= (remainder y 2) 0))
+                               (and (= (remainder x 2) 1) (= (remainder y 2) 1))
                                ))
 #| END |#
