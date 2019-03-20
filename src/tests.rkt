c1d948e4c0bf98b3edd7cca6cb80bf6f158458cb
Kirill Mokevnin <mokevnin@gmail.com>
2019-03-20 10:22:46 +0400

    fix tests

--- src/tests.rkt
+++ src/tests.rkt
@@ -15,13 +15,10 @@ EOF
   (parameterize ([current-namespace (make-base-namespace)])
     (load path)))
 
-(define (generate-message expected actual)
-  (format message-template expected actual))
-
 (define (assert-output expected)
   (let* ([index-path (build-path (current-directory) "index.rkt")]
-          [out (string-trim (with-output-to-string (lambda () (load-solution index-path))))])
-    (displayln out)
-    (cond [(not (string=? out expected))
-           (displayln (generate-message expected out))
+         [actual (string-trim (with-output-to-string (lambda () (load-solution index-path))))])
+    (displayln actual)
+    (cond [(not (string=? actual expected))
+           (displayln (format message-template actual expected))
            (exit 1)])))
