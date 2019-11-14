0db8e36e9a6506e1f30b5bee3e728639769e5145
alexxis <alexxisgreat@gmail.com>
2019-11-14 19:47:06 +0300

    fix the condition of the teacher's solution

--- modules/30-logic/40-cond/index.rkt
+++ modules/30-logic/40-cond/index.rkt
@@ -6,6 +6,6 @@
 (define (programmer-level points-count)
  (cond
     [(< points-count 10) "junior"]
-    [(and (> points-count 10) (< points-count 20)) "middle"]
+    [(and (>= points-count 10) (< points-count 20)) "middle"]
     [else "senior"]))
 #| END |#
